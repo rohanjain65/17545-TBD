c4e44fe5a7b497546de9105642aa945a348ef914
rohanjain65 <rohanjain@utexas.edu>
2024-10-01 13:40:22 -0500

    create backend files

---
+++ login_backend.py
@@ -0,0 +1,3 @@
+# This file is intended to support new user registration and existing user login and interacting with the database
+# it should also encrypt/decrypt passwords
+
---
+++ resource_backend.py
@@ -0,0 +1,4 @@
+# This file is intendend to be used as a backend for the resource server
+# It provides the necessary functions to interact with the databas such as 
+# checkout and checkin of resources, project information
+
